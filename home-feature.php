aa0ad1d0724eddefb47b9b36f88bbbb8d48e653f
miguel <lm.cabanzo@gmail.com>
2011-12-02 18:50:20 +0100

    events page
    basic styling of columns and fonts use

--- home-feature.php
+++ home-feature.php
@@ -6,7 +6,7 @@
 			'title'	=> trim(strip_tags( $post->post_title )),
 			);
      	$thumbID = get_the_post_thumbnail( $post->ID, 'background', $imgsrcparam ); ?>
-         <div><?php echo "$thumbID"; ?></div>
+         <span><?php echo "$thumbID"; ?></span>
      	<?php } ?>
 
      <h2><?php the_title(); ?></h2>
