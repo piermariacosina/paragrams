97661ac2b414ea651590df0e41b92519e869cf87
Riccardo Strobbia <holdwine@gmail.com>
2011-12-05 12:42:22 +0100

    Added message box

--- js/js.js
+++ js/js.js
@@ -2,5 +2,17 @@ var jq = jQuery, Siteinfo;
 
 jq(function( $ )
 {
-$.backstretch(Siteinfo.site_url+"/images/bkgr_img.jpg");
+	jq( "#messages_to_user" ).dialog({ 
+		autoOpen: false
+		});
+	$.backstretch(Siteinfo.site_url+"/images/bkgr_img.jpg");
 });
+
+function globalMessages( message )
+{
+	jq( "#messages_to_user" ).bind( "dialogopen", function(event, ui) 
+	{
+		jQuery(this).html(message);
+	});
+	jq( "#messages_to_user" ).dialog('open');
+};
