92ace66afec38127d7ca8116cc851b0ce9e9b2d0
Piermaria Cosina <piermaria.cosina@gmail.com>
2011-12-13 20:07:08 +0100

    refix testimonial in event-single.php
    added template from the plugin

--- plugins/events-manager/templates/event-single.php
+++ plugins/events-manager/templates/event-single.php
@@ -13,4 +13,42 @@ if( $EM_Event->status == 1 ){
 }else{
 	echo get_option('dbem_no_events_message');
 }
-?>
+
+if(testimonial_current_user_can_write_event($EM_Event->event_owner))
+    bp_testimonials_post_form();
+ 
+$args = array(
+                'user_id'=> $EM_Event->event_owner 
+        );
+ 
+if(bp_has_testimonials($args)):?>
+<div id="pag-top" class="pagination">
+ 
+		<div class="pag-count" id="testimonial-count-top">
+ 
+			<?php bp_testimonials_pagination_count(); ?>
+ 
+		</div>
+ 
+		<div class="pagination-links" id="testimonial-pag-top">
+ 
+			<?php bp_testimonials_pagination_links(); ?>
+ 
+		</div>
+ 
+	</div>
+ 
+	<?php do_action( 'bp_before_testimonials_list' ); ?>
+<ul id="testimonial-list" class="item-list" role="main">
+<?php while(bp_testimonials()):bp_the_testimonial();
+ 
+        bp_testimonials_load_template('testimonials/entry.php')
+?>  
+ 
+<?php endwhile;?>
+</ul>
+<?php else:?>
+<div class="error" id="message">
+    <?php _e('There is no testimonial available at the moment!','testimonials');?>
+</div>
+<?php endif;?>
